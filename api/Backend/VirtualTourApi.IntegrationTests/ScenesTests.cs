b8f4387a0123ed39585d0b1a8d05227d4a4f919a
pavonism <spawlonka@outlook.com>
2023-11-10 21:57:23 +0100

    Prepare some mocks and add more tests

--- api/Backend/VirtualTourApi.IntegrationTests/ScenesTests.cs
+++ api/Backend/VirtualTourApi.IntegrationTests/ScenesTests.cs
@@ -39,16 +39,71 @@ namespace VirtualTourApi.IntegrationTests
             {
                 TourId = _tour.Id,
             };
+            var result = await _service.CreateScene(createSceneParameters);
+
+            result.Scene.Should().NotBeNull();
+            result.Scene.Id.Should().NotBeNull();
+
+            var getTourParameters = new GetTourParameters()
+            {
+                TourId = _tour.Id,
+            };
+            var getTourResult = await _service.GetTourById(getTourParameters);
+            getTourResult.Tour.Should().NotBeNull();
+            getTourResult.Tour.Scenes
+                .Should().NotBeNull()
+                .And.Subject.Should().Contain(result.Scene);
+
+            var deleteSceneParameters = new DeleteSceneParameters() 
+            { 
+                TourId = _tour.Id,
+                SceneId = result.Scene.Id 
+            };
+            await _service.DeleteScene(deleteSceneParameters);
+
+            getTourResult = await _service.GetTourById(getTourParameters);
+            getTourResult.Tour.Should().NotBeNull();
+            getTourResult.Tour.Scenes
+                .Should().NotBeNull()
+                .And.Subject.Should().NotContain(result.Scene);
+        }
+
+        [TestMethod]
+        public async Task CreateSceneThenAddPhotoThenDelete()
+        {
+            var createSceneParameters = new CreateSceneParameters()
+            {
+                TourId = _tour.Id,
+            };
 
             var result = await _service.CreateScene(createSceneParameters);
 
             result.Scene.Should().NotBeNull();
             result.Scene.Id.Should().NotBeNull();
 
-            var deleteSceneParameters = new DeleteSceneParameters() 
-            { 
+            var addPhotoParameters = new AddPhotoToSceneParameters()
+            {
                 TourId = _tour.Id,
-                SceneId = result.Scene.Id 
+                SceneId = result.Scene.Id,
+                PhotoInBytes = new byte[1000],
+            };
+
+            await _service.AddPhotoToScene(addPhotoParameters);
+
+            var getTourParameters = new GetTourParameters()
+            {
+                TourId = _tour.Id,
+            };
+            var getTourResult = await _service.GetTourById(getTourParameters);
+            getTourResult.Tour.Should().NotBeNull();
+            getTourResult.Tour.Scenes
+                .Should().NotBeNull()
+                .And.Subject.Should().Contain(s => s.Id == result.Scene.Id && !string.IsNullOrWhiteSpace(s.Photo360Url));
+
+            var deleteSceneParameters = new DeleteSceneParameters()
+            {
+                TourId = _tour.Id,
+                SceneId = result.Scene.Id
             };
             await _service.DeleteScene(deleteSceneParameters);
         }
