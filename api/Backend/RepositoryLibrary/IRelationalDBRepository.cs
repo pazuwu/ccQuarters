55e4ee6d6de4d4f0b2053d3332175d51957c76e5
Dejniel01 <wujkowskidaniel01@gmail.com>
2023-12-03 14:15:23 +0100

    Add repository layer

---
+++ api/Backend/RepositoryLibrary/IRelationalDBRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace RepositoryLibrary
+{
+    public interface IRelationalDBRepository
+    {
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null);
+        Task<T> QueryFirstAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null);
+        Task<T?> QueryFirstOrDefaultAsync<T>(string sql, object? param = null, IDbTransaction? transaction = null);
+        Task<int> ExecuteAsync(string sql, object? param = null, IDbTransaction? transaction = null);
+
+
+        IDbTransaction BeginTransaction();
+        void CommitTransaction(IDbTransaction transaction);
+        void RollbackTransaction(IDbTransaction transaction);
+    }
+}
