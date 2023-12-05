76ce58085d7ce0797aec38f54680864326c0e869
pavonism <spawlonka@outlook.com>
2023-12-05 18:01:17 +0100

    Move download executor parameters to separate class

--- api/Backend/VirtualTourProcessingAPI/OperationExecutors/Interfaces/IDownloadExecutor.cs
+++ api/Backend/VirtualTourProcessingAPI/OperationExecutors/Interfaces/IDownloadExecutor.cs
@@ -1,7 +1,14 @@
 ﻿namespace VirtualTourProcessingServer.OperationExecutors.Interfaces
 {
+    public class DownloadParameters
+    {
+        public required string TourId { get; set; }
+        public required string AreaId { get; set; }
+        public required string OutputDirectory { get; set; }
+    }
+
     public interface IDownloadExecutor
     {
-        Task<ExecutorResponse> DownloadPhotos(string tourId, string areaId, string outputDirectory);
+        Task<ExecutorResponse> DownloadPhotos(DownloadParameters parameters);
     }
 }
