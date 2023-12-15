63a4e0f98aa27951c9e233d0bf7af8e9bf039e0c
pavonism <spawlonka@outlook.com>
2023-12-15 22:08:46 +0100

    Add compression before post new scene, tweak scenes list a bit

--- client/lib/virtual_tour/service/requests/post_scene_request.dart
+++ client/lib/virtual_tour/service/requests/post_scene_request.dart
@@ -2,20 +2,24 @@ import 'dart:convert';
 
 class PostSceneRequest {
   final String? parentId;
+  final String name;
 
   PostSceneRequest({
     this.parentId,
+    required this.name,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'parentId': parentId,
+      'name': name,
     };
   }
 
   factory PostSceneRequest.fromMap(Map<String, dynamic> map) {
     return PostSceneRequest(
       parentId: map['parentId'] != null ? map['parentId'] as String : null,
+      name: map['name'] as String,
     );
   }
 
