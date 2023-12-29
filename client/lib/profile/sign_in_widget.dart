a76aafd9fb721a7fd1d034846bf92cafbf69ef59
Łucja Żukowska <01161598@pw.edu.pl>
2023-12-29 18:07:09 +0100

    Adjust sign in widget

--- client/lib/profile/sign_in_widget.dart
+++ client/lib/profile/sign_in_widget.dart
@@ -1,3 +1,4 @@
+import 'package:ccquarters/common/consts.dart';
 import 'package:ccquarters/login_register/cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,13 +8,78 @@ class SignInWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        child: const Text('Zaloguj się'),
-        onPressed: () {
-          context.read<AuthCubit>().signOut();
-        },
+    return Padding(
+      padding: const EdgeInsets.all(largePaddingSize),
+      child: Center(
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: MediaQuery.of(context).orientation == Orientation.portrait
+              ? Padding(
+                  padding: const EdgeInsets.all(largePaddingSize),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      _buildTitle(),
+                      const SizedBox(height: 24),
+                      _buildImage(context),
+                      const SizedBox(height: 24),
+                      _buildSignInButton(context),
+                    ],
+                  ),
+                )
+              : Center(
+                  child: Row(
+                    children: [
+                      _buildImage(context),
+                      const SizedBox(width: 24),
+                      Column(
+                        children: [
+                          _buildTitle(),
+                          const SizedBox(height: 24),
+                          _buildSignInButton(context),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+        ),
       ),
+    );
+  }
+
+  Text _buildTitle() {
+    return const Text(
+      'Wygląda na to, że nie\n jesteś zalogowany...',
+      style: TextStyle(
+        fontSize: 30,
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Image _buildImage(BuildContext context) {
+    return Image.asset(
+      'assets/graphics/question.png',
+      height: MediaQuery.of(context).size.height * 0.3,
+    );
+  }
+
+  TextButton _buildSignInButton(BuildContext context) {
+    return TextButton(
+      child: Text(
+        'Zaloguj się lub zarejestruj',
+        style: Theme.of(context)
+            .textTheme
+            .labelLarge
+            ?.copyWith(color: Colors.blueGrey),
+        textScaler: const TextScaler.linear(1.3),
+      ),
+      onPressed: () {
+        context.read<AuthCubit>().signOut();
+      },
     );
   }
 }
