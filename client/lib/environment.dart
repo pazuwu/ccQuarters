6da35b1fd9f4b96097c67982197006fac6e3f717
pavonism <spawlonka@outlook.com>
2024-01-22 00:12:01 +0100

    Add firebase deploy, remove unnecessary firebase config

--- client/lib/environment.dart
+++ client/lib/environment.dart
@@ -4,10 +4,10 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class Environment {
   static String get filename {
     if (kReleaseMode) {
-      return "env/.env.production";
+      return "env/env.production";
     }
 
-    return "env/.env.dev";
+    return "env/env.dev";
   }
 
   static String get apiUrl =>
@@ -22,12 +22,6 @@ class Environment {
   static String get authDomain =>
       dotenv.env["AUTH_DOMAIN"] ?? "AUTH_DOMAIN not found!";
 
-  static String get databaseUrl =>
-      dotenv.env["DATABASE_URL"] ?? "DATABASE_URL not found!";
-
-  static String get storageBucket =>
-      dotenv.env["STORAGE_BUCKET"] ?? "STORAGE_BUCKET not found!";
-
   static String get apiKeyWeb =>
       dotenv.env["API_KEY_WEB"] ?? "API_KEY_WEB not found!";
 
@@ -37,9 +31,6 @@ class Environment {
   static String get messagingSenderIdWeb =>
       dotenv.env["MESSAGING_SENDER_ID_WEB"] ??
       "MESSAGING_SENDER_ID_WEB not found!";
-
-  static String get measurementIdWeb =>
-      dotenv.env["MEASUREMENT_ID_WEB"] ?? "MEASUREMENT_ID_WEB not found!";
 
   static String get apiKeyMobile =>
       dotenv.env["API_KEY_MOBILE"] ?? "API_KEY_MOBILE not found!";
